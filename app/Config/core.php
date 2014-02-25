b3c62d8c8f7703c2284ff572fa9fc944ccbfcfd2
Samuel Debruyn <s@muel.be>
2014-02-25 09:07:08 +0100

    bugfix

--- app/Config/core.php
+++ app/Config/core.php
@@ -17,8 +17,6 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-/*DebugKit Github*/
-    Configure::write('debug', 1);
 /**
  * CakePHP Debug Level:
  *
